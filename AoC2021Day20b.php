a988d751e1f20547dff0a0a0b3db934860caf413
Rien Sach <rien.sach@btinternet.com>
2021-12-20 09:09:06 +0000

    Minor updates

--- AoC2021Day20b.php
+++ AoC2021Day20b.php
@@ -62,30 +62,21 @@ for ($x = 0; $x < $maxSteps; $x++) {
     foreach($infoArray as $key => $value) {
         foreach($value as $key2 => $value2) {
 
-            $stringValue1 = $infoArray[$key-1][$key2-1] ?? '.';
-            $stringValue2 = $infoArray[$key-1][$key2] ?? '.';
-            $stringValue3 = $infoArray[$key-1][$key2+1] ?? '.';
-            $stringValue4 = $infoArray[$key][$key2-1] ?? '.';
-            $stringValue5 = $infoArray[$key][$key2] ?? '.';
-            $stringValue6 = $infoArray[$key][$key2+1] ?? '.';
-            $stringValue7 = $infoArray[$key+1][$key2-1] ?? '.';
-            $stringValue8 = $infoArray[$key+1][$key2] ?? '.';
-            $stringValue9 = $infoArray[$key+1][$key2+1] ?? '.';
+            $stringValue1 = isset($infoArray[$key-1][$key2-1]) ? $infoArray[$key-1][$key2-1]:(($x % 2 == 1) ? '#':'.');
+            $stringValue2 = isset($infoArray[$key-1][$key2]) ? $infoArray[$key-1][$key2]:(($x % 2 == 1) ? '#':'.');
+            $stringValue3 = isset($infoArray[$key-1][$key2+1]) ? $infoArray[$key-1][$key2+1]:(($x % 2 == 1) ? '#':'.');
+            $stringValue4 = isset($infoArray[$key][$key2-1]) ? $infoArray[$key][$key2-1]:(($x % 2 == 1) ? '#':'.');
+            $stringValue5 = isset($infoArray[$key][$key2]) ? $infoArray[$key][$key2]:(($x % 2 == 1) ? '#':'.');
+            $stringValue6 = isset($infoArray[$key][$key2+1]) ? $infoArray[$key][$key2+1]:(($x % 2 == 1) ? '#':'.');
+            $stringValue7 = isset($infoArray[$key+1][$key2-1]) ? $infoArray[$key+1][$key2-1]:(($x % 2 == 1) ? '#':'.');
+            $stringValue8 = isset($infoArray[$key+1][$key2]) ? $infoArray[$key+1][$key2]:(($x % 2 == 1) ? '#':'.');
+            $stringValue9 = isset($infoArray[$key+1][$key2+1]) ? $infoArray[$key+1][$key2+1]:(($x % 2 == 1) ? '#':'.');
 
             $string = $stringValue1.$stringValue2.$stringValue3.$stringValue4.$stringValue5.$stringValue6.$stringValue7.$stringValue8.$stringValue9;
 
             $binString = bindec(str_replace( ['.', '#'], [0, 1], $string));
             $tempArray[$key][$key2] = $imageAlgorithm[$binString];
 
-            if($key==$gridMinWidth && $x % 2 == 1) {
-                $tempArray[$key][$key2] = '.';
-            }
-            if($key2==$gridMaxLength && $x % 2 == 1) {
-                $tempArray[$key][$key2] = '.';
-            }
-            if($key2==$gridMinWidth && $key==$gridMaxLength && $x % 2 == 1) {
-                $tempArray[$key][$key2] = '.';
-            }
         }
     }
     $infoArray = $tempArray;
@@ -101,7 +92,6 @@ for ($x = 0; $x < $maxSteps; $x++) {
     $infoArray = $tempArray;
 }
 
-
 //printGrid($infoArray);
 
 $lightPixelsPartB = 0;
@@ -115,7 +105,6 @@ foreach($infoArray as $key => $value) {
 
 echo "Day 20 Part A: Total lit pixels ".$lightPixelsPartA."<br><br>\n";
 echo "Day 20 Part B: Total lit pixels ".$lightPixelsPartB."<br><br>\n";
-
 
 $time_post = microtime(true);
 $exec_time = $time_post - $time_pre;
